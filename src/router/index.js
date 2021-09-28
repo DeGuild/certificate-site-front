beba4f36e1535196e97f3cc7ee97eee7805c8728
parmcoder <possawat2017@gmail.com>
2021-09-28 08:40:30 +0700

    Starting on DG-47

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,6 @@ const routes = [
 const router = new VueRouter({
   routes,
   mode: 'history',
-
 });
 
 export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
-  <hello-world />
+  <div><connect-wallet /> <hello-world /></div>
 </template>
 
 <script>
 import HelloWorld from '../components/HelloWorld.vue';
+import ConnectWallet from '../components/ConnectWallet.vue';
 
 export default {
   name: 'Home',
 
   components: {
     HelloWorld,
+    ConnectWallet,
   },
 };
 </script>
